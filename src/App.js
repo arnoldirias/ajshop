8acf273a27075cd48075aaa3e74075ba5ee2da63
arnoldirias <arnold.irias@unah.hn>
2021-02-17 16:08:14 -0600

    Add firestore dependencies2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-import firebase from "firebase/app";
+//import firebase from "firebase/app";
 
 // Add the Firebase services that you want to use
 //import "firebase/auth";
-import "firebase/firestore";
+//import "firebase/firestore";
 
-import firebaseConfig from './firebaseConfig';
+//import firebaseConfig from './firebaseConfig';
 
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
 
 function App() {
 
