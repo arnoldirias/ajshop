d3375c5a3508d5ab6200566f71fde626860ac0cc
arnoldirias <arnold.irias@unah.hn>
2021-02-17 15:57:43 -0600

    Add firestore dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+//import "firebase/auth";
+import "firebase/firestore";
+
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+import firebaseConfig from './firebaseConfig';
+         
         </p>
         <a
           className="App-link"
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+export default {
+   
+    apiKey: "AIzaSyDxIRoJf1ZXW3nonigEsXCkp3Gcoxa9kRE",
+    authDomain: "ajdemo-f8ee0.firebaseapp.com",
+    databaseURL: "https://ajdemo-f8ee0.firebaseio.com",
+    projectId: "ajdemo-f8ee0",
+    storageBucket: "ajdemo-f8ee0.appspot.com",
+    messagingSenderId: "968951711696",
+    appId: "1:968951711696:web:2d68a8eefed19afd581bd5",
+    measurementId: "G-NVHF2J8752"
+    
+  }
+
+
+  
